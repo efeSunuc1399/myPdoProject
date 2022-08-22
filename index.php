b81b4df73b5079f7c9f0168232823f91e9b29fb0
Yusuf <yusuflacinn1@gmail.com>
2022-08-22 12:52:12 +0300

    fix: loaded script using script src

--- index.php
+++ index.php
@@ -5,12 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/bootstrap.min.css">
-    <link rel="stylesheet" href="scripts/bootstrap.min.js">
     <title>Document</title>
 </head>
 <body>
 <form action="" method="post">
-
+<script>src="scripts/bootstrap.min.js"</script>
 </form>
 </body>
 </html>
